f78ccbec78877966d84b33673a6e28a620bff07f
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-05-11 16:28:24 -0500

    using p5 stuff inside other class

--- src/ball.ts
+++ src/ball.ts
@@ -1,7 +1,17 @@
-export default class Ball {
-  public posX: number | undefined;
+import p5 from 'p5';
 
-  constructor(posX: number) {
+export default class Ball {
+  public posX: number;
+
+  public p: p5;
+
+  constructor(posX: number, p:p5) {
     this.posX = posX;
+    this.p = p;
+  }
+
+  draw(): void {
+    this.p.fill(255, 255, 0);
+    this.p.ellipse(this.posX, 200, 50, 50);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,18 @@ import './style.css';
 import p5 from 'p5';
 import Ball from './ball';
 
-const myBall = new Ball(34);
-console.log(myBall);
-
 const sketch = (p:p5) => {
+  const myBall = new Ball(34, p);
   // eslint-disable-next-line no-param-reassign
   p.setup = () => {
+    // eslint-disable-next-line no-console
+    console.log(myBall);
     p.createCanvas(500, 500);
   };
   // eslint-disable-next-line no-param-reassign
   p.draw = () => {
     p.background(0);
-    p.fill(255, 255, 0);
-    p.ellipse(200, 200, 50, 50);
+    myBall.draw();
   };
 };
 // eslint-disable-next-line new-cap
